1b561f854170e571b27e8272fa62ce65c11e032d
R9han <130710997+R9han@users.noreply.github.com>
2025-07-22 01:32:14 +0600

    Add files via upload

---
+++ string, int, float, boolean.py
@@ -0,0 +1,32 @@
+#string
+massage = "good boy"
+distance = "32km"
+
+print(f"rohan is a {massage}")
+print(f"my house is far from my school at least {distance}")
+
+
+#integer
+num = 88001732884011
+distance = 8367
+gpa = 5
+
+print(f"my phone number is {num}")
+print(f"i am {distance} km away")
+print(f"my gpa is {gpa}")
+
+
+#Float
+distance = 3.4
+gpa = 3.3
+price = 34.33
+
+print(f"the price is {price}")
+print(f"my gpa {gpa}")
+print(f"you ran {distance} mile")
+
+
+# Boolean
+is_student = True
+for_sale = False
+is_online = True
