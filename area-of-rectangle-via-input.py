809b442713d1cec461d8e361e2d9fc55166e89e0
Sahriar Tanjim Rohan <tungtungsaur321@gmail.com>
2025-07-22 22:18:58 +0600

    Add files via upload

---
+++ area-of-rectangle-via-input.py
@@ -0,0 +1,8 @@
+ # Exercise 1 Rectangle Area Calculations 
+
+length = int(input("Enter the length: ")) # its better to use float in this case 
+width  = int(input("Enter the width: "))  # using the int just for fun :) 
+
+area = length * width 
+
+print(f"The area is {area}")
