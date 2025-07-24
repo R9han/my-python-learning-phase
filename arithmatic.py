40c33c875411698138ba8f25f5e097ec6cc12dfb
Sahriar Tanjim Rohan <tungtungsaur321@gmail.com>
2025-07-25 04:41:14 +0600

    Add files via upload

---
+++ arithmatic.py
@@ -0,0 +1,21 @@
+students = 10 
+
+# students = students + 1 
+# students += 1   
+
+# students = students - 6
+# students -= 6 
+
+# students = students * 3 
+# students *= 3 
+
+# students = students / 3      "/" > this is division sign 
+# students /= 3   
+
+students = students ** 3 
+# students **= 3  
+
+# remainder = students % 3 
+# remainder %= 3 
+
+print(students)
---
+++ function-and-math.py
@@ -0,0 +1,8 @@
+x = 3.1415 
+y = -4 
+z = 8 
+
+# result = round(x)      >> round() means the round figure of.... 
+result = abs(y)        
+
+print(result)
