e209e75fafcaee669c8b975fb6a781c0be6d34bc
Sahriar Tanjim Rohan <tungtungsaur321@gmail.com>
2025-07-24 23:59:32 +0600

    Add files via upload

---
+++ phython.py
@@ -0,0 +1,42 @@
+print("Hello world") 
+
+# these are variables
+
+name = "rodela"
+age = 15
+price = 8.66
+is_student = True
+good_cooker = True
+
+print("my name is rodela.my age is 15")
+print(33+89)
+print(name, age ,price)
+print(f"hi there {name}")
+
+
+
+#Convert a string or number to a floating-point number, if possible.
+
+age =str (age)
+print(age)
+name = bool(name)
+print(name)                        #this is typecasting
+
+
+#input() = a function that prompts the user to enter data and returns the entered data as a str
+
+
+
+
+name = input("hello.what is your name?: ")
+age = int(input("how old are you?: "))
+hobbie = input("whats your favourite hobbie?: ")
+
+age = age + 1
+
+print(f"hello {name}!")
+print("asssalamualaykum!")
+print(f"wow! you are {age} years old and i also like to play {hobbie}")
+
+
+
