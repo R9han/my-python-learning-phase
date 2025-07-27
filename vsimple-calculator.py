37bc836b0f8bac6f7171bd5397c69ac1e139ecb9
Sahriar Tanjim Rohan <tungtungsaur321@gmail.com>
2025-07-27 22:05:44 +0600

    Add files via upload

---
+++ vsimple-calculator.py
@@ -0,0 +1,23 @@
+operator = input('Enter an operator (+, -, *, /): ')
+num1 = float(input("Enter num1: "))
+num2 = float(input("Enter num2: "))
+
+if operator == "+": 
+     result = num1 + num2
+     print(round(result, 3))
+
+elif operator == "-": 
+     result = num1 - num2
+     print(round(result, 3)) 
+
+elif operator == "*": 
+    result = num1 * num2
+    print(round(result, 3)) 
+
+elif operator == "/": 
+    result = num1 / num2
+    print(round(result, 3))
+else:
+     print(f"{operator} is not a valid operator ") 
+ 
+     
