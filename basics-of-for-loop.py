ba4977acc96bbeb732a96f5470376f7ef1c1837e
Sahriar Tanjim Rohan <tungtungsaur321@gmail.com>
2025-08-07 23:22:55 +0600

    Add files via upload

---
+++ basics-of-for-loop.py
@@ -0,0 +1,21 @@
+# for x in reversed(range(1,11)):
+    # print(x)
+
+# print("ye act of god nehi, ye to act of fraud hain fraud ")    
+
+# credit_number = "123-456-789-1011-123"
+
+# for x in credit_number:
+    # print(x)
+
+# for x in range(1, 11): 
+    # if x == 9:
+        # continue
+    # else:
+        # print(x)
+
+for x in range(1, 11): 
+    if x == 5:
+        break
+    else:
+        print(x)        
